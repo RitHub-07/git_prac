0f5e240cbc8b5f916cba1e657a253e3813af4915
ritul <rituldingora1@gmail.com>
2025-04-03 12:24:04 +0530

    third commit

--- 1.py
+++ 1.py
@@ -1,2 +1,14 @@
 print("gjkjl")
 print("vhcgfydtcfgvjbhnj")
+
+def sqr(x):
+    return x * x
+print(sqr(5))
+
+def Addition(*args):
+   print(sum(args))
+print(Addition(1, 2, 3, 4))
+
+tup=(2,1,2,3,4,2,6)
+print(tup.count(2))
+print(tup.index(3))
